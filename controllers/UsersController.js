c4f6f89120671fd70b261f75e90e86c763235d5f
Nimo08 <nimomohamed800@gmail.com>
2024-04-14 15:49:49 -0700

    Restructured postNew & index.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,32 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+async function postNew(req, res) {
+  const { email, password } = req.body;
 
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      return;
-    }
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      return;
-    }
-
-    const users = dbClient.db.collection('users');
-    await users.findOne({ email }, (err, result) => {
-      if (result) {
-        res.status(400).json({ error: 'Already exist' });
-      } else {
-        const hashedPasswd = sha1(password);
-        users.insertOne({ email, password: hashedPasswd }).then((user) => {
-          res.status(201).json({ id: user.insertedId, email });
-        });
-      }
-    });
+  if (!email) {
+    res.status(400).json({ error: 'Missing email' });
+    return;
   }
+  if (!password) {
+    res.status(400).json({ error: 'Missing password' });
+    return;
+  }
+  if (!dbClient.isAlive()) {
+    throw new Error('Database connection is not established.');
+  }
+  const userExist = await dbClient.dbClient.collection('users').findOne({ email });
+  if (userExist) {
+    res.status(400).json({ error: 'Already exist' });
+  }
+  const hashedPasswd = sha1(password);
+  const user = {
+    email,
+    password: hashedPasswd,
+  };
+  const result = await dbClient.dbClient.collection('users').insertOne(user);
+  const { _id, email: _email } = result.ops[0];
+  res.status(201).json({ id: _id, email: _email });
 }
 
-module.exports = UsersController;
+module.exports = { postNew };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+
+const { postNew } = require('../controllers/UsersController');
 
 const router = Router();
 
@@ -10,24 +9,6 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
-router.post('/users', UsersController.postNew);
-
-router.get('/users/me', UsersController.getMe);
-
-router.get('/connect', AuthController.getConnect);
-
-router.get('/disconnect', AuthController.getDisconnect);
-
-router.post('/files', FilesController.postUpload);
-
-router.get('/files/:id', FilesController.getShow);
-
-router.get('/files', FilesController.getIndex);
-
-router.put('/files/:id/publish', FilesController.putPublish);
-
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-router.get('/files/:id/data', FilesController.getFile);
+router.post('/users', postNew);
 
 module.exports = router;
