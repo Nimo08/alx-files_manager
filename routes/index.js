ef762ae7ee2ffa74e1ee68ef94cf1daad2e6bbd4
Nimo08 <nimomohamed800@gmail.com>
2024-04-15 01:05:55 -0700

    Add FilesController routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 
+import FilesController from '../controllers/FilesController';
+
 const { postNew, getMe } = require('../controllers/UsersController');
 const { getConnect, getDisconnect } = require('../controllers/AuthController');
 
@@ -17,5 +19,17 @@ router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 
 router.get('/users/me', getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
